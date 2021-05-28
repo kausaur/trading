75e556dd129c9453b25bddeecdd1c03d19d9a6ca
kausaur <kaushiksaurabh17@yahoo.com>
2021-05-29 00:41:57 +0530

    correcting the DB name

--- trading/database/dbsettings.py
+++ trading/database/dbsettings.py
@@ -1,1 +1,1 @@
-DATABASE = {"default": {"HOST": "localhost", "PASSWORD": "xp2#$kal1", "ENGINE": "django.db.backends.postgresql", "USER": "postgres", "NAME": "trading", "PORT": "5432"}}
+DATABASE = {"default": {"HOST": "localhost", "PASSWORD": "xp2#$kal1", "ENGINE": "django.db.backends.postgresql", "USER": "postgres", "NAME": "tradingDB", "PORT": "5432"}}
